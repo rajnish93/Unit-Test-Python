26873230caba4f46568af8a627f790bbbd3a3017
byionline <10815566+byionline@users.noreply.github.com>
2021-06-07 16:20:33 +0530

    Test all cases

--- test_calculator.py
+++ test_calculator.py
@@ -12,16 +12,26 @@ class TestCal(unittest.TestCase):
         self.assertEqual(calculator.add(-30,15),-15)
     ## Test Subtract Function
     def test_sub(self):
-        result = calculator.sub(30,15)
-        self.assertEqual(result,15)
+        # result = calculator.sub(30,15)
+        self.assertEqual(calculator.sub(30,15),15)
+        self.assertEqual(calculator.sub(30,-15),45)
+        self.assertEqual(calculator.sub(-30,-15),-15)
+        self.assertEqual(calculator.sub(-30,15),-45)
     ## Test Multiplication Function
     def test_mul(self):
-        result = calculator.mul(30,15)
-        self.assertEqual(result,450)
+        # result = calculator.mul(30,15)
+        self.assertEqual(calculator.mul(30,15),450)
+        self.assertEqual(calculator.mul(30,-15),-450)
+        self.assertEqual(calculator.mul(-30,-15),450)
+        self.assertEqual(calculator.mul(-30,15),-450)
     ## Test Division Function
     def test_div(self):
-        result = calculator.div(30,15)
-        self.assertEqual(result,2)
+        # result = calculator.div(30,15)
+        self.assertEqual(calculator.div(30,15),2)
+        self.assertEqual(calculator.div(30,-15),-2)
+        self.assertEqual(calculator.div(-30,-15),2)
+        self.assertEqual(calculator.div(30,1.5),20)
+        self.assertRaises(ValueError, calculator.div, 10,0)
 """
  Run code using:
  python -m unittest test_calculator.py
