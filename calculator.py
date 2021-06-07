420674f59de86d0a3f224246b39d10390a961830
byionline <10815566+byionline@users.noreply.github.com>
2021-06-07 15:32:26 +0530

    Calculator Functions

---
+++ calculator.py
@@ -0,0 +1,15 @@
+""" Add Function """
+def add(x,y):
+    return x+y
+""" Subtract Function """
+def sub(x,y):
+    return x-y
+""" Multiplication Function """
+def mul(x,y):
+    return x*y
+""" Division Function """
+def div(x,y):
+    if y==0:
+        raise ValueError('Division by zero not allowed.')
+    return x/y
+
